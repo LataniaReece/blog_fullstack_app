89a222684f93e78e43681c6182e09360120f9237
“Latania <latania.reece@outlook.com>
2024-02-28 07:39:23 -0500

    add mobile handle link click and change mobile bg color for navbar

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -12,11 +12,17 @@ import logo from "../images/logo-white.png";
 const Navbar: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { id, username } = useSelector((state: RootState) => state.user);
 
   const handleLogout = () => {
     dispatch(logUserOut());
+  };
+
+  const handleLinkClick = (path: string) => {
+    navigate(path);
+    setIsOpen(false);
   };
 
   const navVariants = {
@@ -63,10 +69,13 @@ const Navbar: FC = () => {
 
       {/* Mobile Menu */}
       <div className="md:hidden flex justify-between items-center">
-        <Link to="/" className="hover:underline flex items-center gap-2">
+        <button
+          onClick={() => handleLinkClick("/")}
+          className="hover:underline flex items-center gap-2"
+        >
           <img src={logo} className="h-8 w-8 rounded-full" />
           Lifestyle Blogs
-        </Link>
+        </button>
         <button onClick={() => setIsOpen(!isOpen)}>
           {/* Hamburger Icon */}
           <div className="flex flex-col space-y-1">
@@ -79,7 +88,7 @@ const Navbar: FC = () => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="md:hidden fixed inset-0 bg-gray-800 flex flex-col items-center space-y-4 pt-16 z-10"
+            className="md:hidden fixed inset-0 bg-black flex flex-col items-center space-y-4 pt-16 z-10"
             initial="closed"
             animate="open"
             exit="closed"
@@ -87,10 +96,13 @@ const Navbar: FC = () => {
             transition={{ type: "spring", stiffness: 300, damping: 30 }}
           >
             <div className="absolute top-0 flex justify-between w-full p-3">
-              <Link to="/" className="hover:underline flex items-center gap-2">
+              <button
+                className="hover:underline flex items-center gap-2"
+                onClick={() => handleLinkClick("/")}
+              >
                 <img src={logo} className="h-8 w-8 rounded-full" />
                 Lifestyle Blogs
-              </Link>
+              </button>
               <button
                 className=" text-white outline-none"
                 onClick={() => setIsOpen(false)}
@@ -98,23 +110,41 @@ const Navbar: FC = () => {
                 <FaTimes size={24} />
               </button>
             </div>
-            {id ? (
-              <>
-                <button onClick={() => handleLogout()}>Logout</button>
-                <Link to="/account" className="hover:underline">
-                  {`${username}'s Account`}
-                </Link>
-              </>
-            ) : (
-              <>
-                <Link to="/login" className="hover:underline">
-                  Login
-                </Link>
-                <Link to="/register" className="hover:underline">
-                  Register
-                </Link>
-              </>
-            )}
+            <>
+              <button
+                onClick={() => handleLinkClick("/blogs/search")}
+                className="hover:underline flex items-center gap-2"
+              >
+                Search
+              </button>
+
+              {id ? (
+                <>
+                  <button
+                    onClick={() => handleLinkClick("/account")}
+                    className="hover:underline capitalize"
+                  >
+                    {`${username}'s Account`}
+                  </button>
+                  <button onClick={() => handleLogout()}>Logout</button>
+                </>
+              ) : (
+                <>
+                  <button
+                    onClick={() => handleLinkClick("/login")}
+                    className="hover:underline"
+                  >
+                    Login
+                  </button>
+                  <button
+                    onClick={() => handleLinkClick("/register")}
+                    className="hover:underline"
+                  >
+                    Register
+                  </button>
+                </>
+              )}
+            </>
           </motion.div>
         )}
       </AnimatePresence>
