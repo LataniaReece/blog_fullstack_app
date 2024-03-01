b234a95966bff7abceafe38c93bee95b26b2352b
“Latania <latania.reece@outlook.com>
2024-03-01 10:41:49 -0500

    fix errorboundary typescript error

--- ui/src/components/ErrorBoundary.tsx
+++ ui/src/components/ErrorBoundary.tsx
@@ -17,6 +17,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     this.state = { hasError: false };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static getDerivedStateFromError(_: Error): ErrorBoundaryState {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
