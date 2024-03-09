c3054307254a368a9a18c9ff8cb5cfffd1983c6d
“Latania <latania.reece@outlook.com>
2024-03-09 15:59:01 -0500

    use cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -7,19 +7,17 @@ import routes from "./routes";
 function createServer() {
   const app: Application = express();
 
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_ORIGIN,
-      credentials: true,
-    })
-  );
+  const corsOptions = {
+    origin: process.env.FRONTEND_ORIGIN,
+    credentials: true,
+  };
 
+  app.use(cors(corsOptions));
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_ORIGIN);
-    res.header("Access-Control-Allow-Credentials", "true");
+    console.log("Frontend Origin:", process.env.FRONTEND_ORIGIN);
+    console.log("Incoming Request Origin:", req.headers.origin);
     next();
   });
-
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
